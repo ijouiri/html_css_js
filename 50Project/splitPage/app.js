f28ab78a86f80ba46865886dfb21d5db3a8d7539
ijouiri <abdellahijouiri@gmail.com>
2022-06-04 19:44:27 +0000

    finish teh splitPage

--- 50Project/splitPage/app.js
+++ 50Project/splitPage/app.js
@@ -1,2 +1,26 @@
+const left = document.querySelector(".left"),
+  right = document.querySelector(".right"),
+  container = document.querySelector(".container");
 
+// ------ method 1 --------
+left.addEventListener("mouseenter", () => { container.classList.add("hover-left") })
+left.addEventListener("mouseleave", () => { container.classList.remove("hover-left") })
+
+right.addEventListener("mouseenter", () => { container.classList.add("hover-right") })
+right.addEventListener("mouseleave", () => { container.classList.remove("hover-right") })
+
+// ------ method 2 --------
+// document.addEventListener("mouseover", (e) => {
+//   let target = e.target;
+
+//   if (target.classList.contains("right")) {
+//     console.log("ye")
+//     container.classList.add("hover-right")
+//     container.classList.remove("hover-left")
+//   } else {
+//     container.classList.add("hover-left")
+//     container.classList.remove("hover-right")
+//   }
+
+// })
 
--- projects/githubFinder/app.js
+++ projects/githubFinder/app.js
@@ -2,26 +2,27 @@
 const github = new Github;
 const ui = new UI;
 
-document.querySelector(".input-form").addEventListener("keyup",e => {
+document.querySelector(".input-form").addEventListener("keyup", e => {
   e.preventDefault();
   const input = e.target.value;
 
-  if (!input == ""){
+  if (!input == "") {
     github.getUser(input)
       .then(user => {
-      if (user.userData.message == "Not Found"){
-        // show alert
-        ui.showAlert("red");
+        console.log(user)
+        if (user.userData.message == "Not Found") {
+          // show alert
+          ui.showAlert("red");
 
-      }else{
-        //show the profile info and the repos
-        ui.showAlert("#66FF00");
-        ui.showProfile(user.userData);
-        ui.showRepos(user.repos);
-        console.log(user);
-      }
-    })
-  }else{
+        } else {
+          //show the profile info and the repos
+          ui.showAlert("#66FF00");
+          ui.showProfile(user.userData);
+          ui.showRepos(user.repos);
+          console.log(user);
+        }
+      })
+  } else {
     // when the imput is empty clear the profile
     ui.clearProfile();
 
